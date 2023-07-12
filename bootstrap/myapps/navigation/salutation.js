96cb90727f39a990f8d10a2f39aeace7fbfc08dc
Banadz <xandrianajorobanadz@gmail.com>
2023-07-12 10:44:31 +0200

    Formalité de la salutation

--- bootstrap/myapps/navigation/salutation.js
+++ bootstrap/myapps/navigation/salutation.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
             if ((agent['PASSWORD'] === "0000") || (agent['PASSWORD'] === 'default')){
                 $('#salutation').modal('show')
                 let formalit = 'Monsieur'
-                if(agent['GENRE'] === 'M'){
+                if(agent['GENRE'] === 'F'){
                     formalit = 'Madame'
                 }
                 let i = 0     
@@ -29,13 +29,14 @@ $(document).ready(function(){
                 })
                 function magimagie(){
                     var place = $('#lettreSalut')
-                    var message = `Bonjour `+formalit+` `+agent['NOM_UTIL_AG']+`, Bienvenu sur GPS, Une application de Gestion du Patrimoine
-                    et de Stock. L'utilisation de cette application nécessite l'utilisation des comptes personneles. Prémièrement, pour relier 
-                    l'application à votre siuation dans le système de gestion physique. Deuxièmement, suivre vos activités au sein de l'application.
-                    Votre comptes sera protégé par une système d'authentification qui est réquis au début de chaque session d'utilisation.
+                    var message = `Bonjour `+formalit+` `+agent['NOM_UTIL_AG']+`, Bienvenu sur CAM-i, Une application de Comptabililté Administrative et des
+                    Matières Informatisées. L'utilisation de cette application nécessite l'utilisation des comptes personneles. Prémièrement, pour relier 
+                    l'application à votre situation dans le système de gestion physique. Deuxièmement, suivre vos activités au sein de l'application. Troisièmement
+                    pour stocker, manipuler, imprimer les données ou information qui vous concerne. Votre comptes sera protégé par une système d'authentification 
+                    qui est réquis au début de chaque session d'utilisation.
                     Comme votre compte est nouvellement créé avec un mot de passe par défaut, c'est pourquioi on vous propose de vérifier la 
                     conformité de vos informations personnels ainsi que de changer votre mot de passe dans le menu profil pour sécurisé votre compte.
-                    Voulez vous changer le mot de passe maintenant?`
+                    Veuillez changez votre mot de passe pour bien sécuriser votre compte.`
                     
                     if (i < message.length){
                         $('#lettreSalut').append(message.charAt(i))
